50c40e9b1435262d21aece98dded588513041413
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-04-19 17:06:49 +0530

    changes according to ajay suggestions

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ var images = new Array(
   "images/pic1.jpeg",
   "images/pic2.jpeg",
   "images/pic3.jpeg"
-);
-var nextimage = 0;
-doSlideshow();
+)
+var nextimage = 0
+if (window.innerWidth > 460) {
+  doSlideshow()
+}
 
 function doSlideshow() {
   if (nextimage >= images.length) {
-    nextimage = 0;
+    nextimage = 0
     // console.log(nextimage);
   }
   $(".mainContent")
     .css("background-image", 'url("' + images[nextimage++] + '")')
     .fadeIn(500, function () {
-      setTimeout(doSlideshow, 5000);
-    });
+      setTimeout(doSlideshow, 5000)
+    })
 }
